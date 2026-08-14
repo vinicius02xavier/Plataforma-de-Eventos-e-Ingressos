801fa11a708b8b00c2d0add699e5cdb5c183de28
Vinicius Xavier <vinicius02xavier@users.noreply.github.com>
2026-08-13 21:38:57 -0300

    render

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -3,11 +3,12 @@ import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
 
 const connectionString = process.env.DATABASE_URL ?? "file:./dev.db";
+const useSqlite = connectionString.startsWith("file:");
 
-const adapter = new PrismaBetterSqlite3({
-  url: connectionString,
-});
-
-export const prisma = new PrismaClient({
-  adapter,
-});
+export const prisma = useSqlite
+  ? new PrismaClient({
+      adapter: new PrismaBetterSqlite3({
+        url: connectionString,
+      }),
+    })
+  : new PrismaClient();
